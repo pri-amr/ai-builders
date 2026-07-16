dda6787f5eeadc2abb1753f1f1f7f93fb56ed52b
psacchi <psacchi@mutualamr.org.ar>
2026-07-16 13:32:09 -0300

    chore: init frontend project

---
+++ frontend/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const config: Config = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["**/*.test.{ts,tsx}"],
+};
+
+export default createJestConfig(config);
